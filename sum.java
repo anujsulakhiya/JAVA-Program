f4f8db0afe996fc8f6aecf18a0a33ccd59cfefef
Anuj Sulakhiya <anujsulakhiya@gmail.com>
2020-04-02 22:56:23 +0530

    simple programs

---
+++ sum.java
@@ -0,0 +1,10 @@
+
+public class sum{
+
+    static int sum(int a,int b){
+        return a+b;
+    }    public static void main(String[] args){
+            int x=sum(2,3);
+            System.out.println(x);
+    }
+}
