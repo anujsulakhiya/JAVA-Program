0de2e3b9d526e4792b34efe9c7ba25d2f383ac3a
Anuj Sulakhiya <anujsulakhiya@gmail.com>
2020-04-03 20:07:08 +0530

    All Information About Java version ,Class path,, vendor

---
+++ javainfo.java
@@ -0,0 +1,13 @@
+
+public class javainfo{
+        public static void main(String[] args){
+
+            System.out.println("Java Version :"+System.getProperty("java.version"));
+            System.out.println("Java Runtiome Version :"+System.getProperty("java.runtime.version"));
+            System.out.println("Java Home :"+System.getProperty("java.home"));
+            System.out.println("Java Vendor :"+System.getProperty("java.vendor"));
+            System.out.println("Java Vendor Url :"+System.getProperty("java.vendor.url"));
+            System.out.println("Java class Path :"+System.getProperty("java.class.path"));
+
+    }
+}
