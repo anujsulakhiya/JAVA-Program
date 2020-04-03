c50b1a387f0f1b87870283d6dc930b9231ddb51c
Anuj Sulakhiya <anujsulakhiya@gmail.com>
2020-04-03 14:25:12 +0530

    Simple Program

--- multiply.java
+++ multiply.java
@@ -1,10 +1,20 @@
+import java.util.*; 
 
 public class multiply{
 
     static int multiply(int a,int b){
         return a*b;
     }    public static void main(String[] args){
-            int x=multiply(2,3);
+            int a,b;
+            Scanner in= new Scanner(System.in);
+
+            System.out.println("Enter First Number :");
+            a=in.nextInt();
+            System.out.println("Enter Second Number :");
+            b=in.nextInt();
+
+
+            int x=multiply(a,b);
             System.out.println(x);
     }
 }
