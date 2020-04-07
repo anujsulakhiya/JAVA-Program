8e00e1e57f8792559377acd05b83f8c6bcbf6367
Anuj Sulakhiya <anujsulakhiya@gmail.com>
2020-04-07 13:25:10 +0530

    Find The ASCII VAlue OF Characters

---
+++ ascii_value_char.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+
+public class ascii_value_char{
+
+       public static void main(String[] args){
+                       Scanner in= new Scanner(System.in);
+
+            System.out.println("Enter One Character :");
+            char a=in.next().charAt(0);
+            
+            System.out.println(a);
+    }
+}
