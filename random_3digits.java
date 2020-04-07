ab9e0192f61e5d3e8734e78fd56169e4b2961585
Anuj Sulakhiya <anujsulakhiya@gmail.com>
2020-04-07 13:24:47 +0530

    Create a 3 digit Random Number and count them

--- random_3digits.java
+++ random_3digits.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class random_3digits{
 
     static int random_3digits(int a,int b,int c){
+        
         int i,j,k,amount=0;
+
         for(i=a;i<=4;i++){
             for(j=b;j<=4;j++){
                 for(k=c;k<=4;k++){
@@ -17,13 +19,16 @@ public class random_3digits{
         System.out.println("Amount Of All Number is " + amount);
         return 0;
     }    public static void main(String[] args){
-            int a,b,c,d;
+
+            int a,b,c;
             Scanner in= new Scanner(System.in);
 
             System.out.println("Enter First Number :");
             a=in.nextInt();
+
             System.out.println("Enter Second Number :");
             b=in.nextInt();
+
             System.out.println("Enter Third Number :");
             c=in.nextInt();
             
